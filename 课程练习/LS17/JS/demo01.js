b9610f2a916c0ea7079d48e1e70a9534598d5000
gaoyimei0721 <1272379222@qq.com>
2018-05-02 20:13:59 +0800

    V17 第十七次提交（2018.5.2）

---
+++ 课程练习/LS17/JS/demo01.js
@@ -0,0 +1,10 @@
+//通过Object.create静态方法创建的对象的原型共享问题
+var superObj = {
+    x:1,
+    y:2
+};
+var subObj_First = Object.create(superObj);
+var subObj_Second = Object.create(superObj);
+subObj_First.__proto__.x = 5; //5
+//subObj_First.x = 5; // 1
+console.log(subObj_Second.x);
